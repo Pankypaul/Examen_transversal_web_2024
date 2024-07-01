95bcaf9d750c1558d6462c670887721afa81a16c
melanie <me.ortiz@duocuc.cl>
2024-07-01 19:09:56 -0400

    footer arreglado y api de noticias

--- pagina/urls.py
+++ pagina/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import noticias
 
 urlpatterns = [    
     path('nav', views.nav, name="nav"),
     path('footer', views.footer, name="footer"),
-    path('base', views.base, name="base")
+    path('base', views.base, name="base"),
+    path('noticias/', noticias, name='noticias'),
 ]
